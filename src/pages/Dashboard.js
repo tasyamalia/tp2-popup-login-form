e3032153c7452f31bef4e1c81b92f375f90b04b0
Tasya Amalia <tasya.salsabila@ist.id>
2023-01-21 19:52:08 +0700

    add popup login form

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+// import { Button, Card, Form } from "react-bootstrap";
+import ReactDOM from "react-dom";
+import IdleTimer, { IdleTimerProvider } from "react-idle-timer";
+ import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+// import withReactContent from 'sweetalert2-react-content'
+
+function Home() {
+  return (
+    <div className="app">
+      <div class="container-login100">
+      <div class="wrap-login100">
+        <h1 className="text-center mt-3 mb-4">Welcome,</h1>
+        <h1 className="text-center mt-3 mb-4">2301968954</h1>
+        <h1 className="text-center mt-3 mb-4">Tasya Amalia Salsabila</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.idleTimer = null;
+    this.handleOnIdle = this.handleOnIdle.bind(this);
+  }
+  render() {
+    return (
+      <div>
+        <IdleTimerProvider
+          ref={(ref) => {
+            this.idleTimer = ref;
+          }}
+          timeout={1000 * 30}
+          onIdle={this.handleOnIdle}
+          debounce={250}
+        />
+        <Home />
+      </div>
+    );
+  }
+
+  handleOnIdle(event) {
+    Swal.fire({
+        title:'Peringatan',
+        html:'Tidak ada kegiatan selama 30 detik, silahkan login kembali',
+        icon:'question',
+        allowOutsideClick:false
+    }
+      ).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            window.open('/', '_self').focus();
+            window.open('/', '_self').focus();
+        } 
+      })
+  }
+}
---
+++ src/pages/Login.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import '../css/main.css'
+import '../css/util.css'
+import {useNavigate } from 'react-router-dom';
+
+export class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isError: false,
+      isWait: false,
+      waitingTime: 0,
+      countError: 0,
+      errorMessage: "",
+      isRecaptchaClicked:false
+    };
+    this._reCaptchaRef = React.createRef();
+  }
+  render() {
+    const handleClickRecaptcha = (event) => {
+      this.setState({ isRecaptchaClicked: true });
+    }
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      var { inputUsername, inputPassword } = document.forms[0];
+      var username = inputUsername.value;
+      var password = inputPassword.value;
+
+      if (username !== "tasyaamalia") {
+        this.setState({ isError: true, errorMessage: "Username salah" , isRecaptchaClicked:false});
+        event.target.reset();
+        this.captcha.reset()
+      } else if (password !== "qwerty123") {
+        this.setState({ isError: true, errorMessage: "Password salah", isRecaptchaClicked:false});
+        event.target.reset();
+        this.captcha.reset()
+      } else if(this.state.isRecaptchaClicked === false){
+        this.setState({ isError: true, errorMessage: "You must confirm you are not a robot" });
+        event.target.reset();
+      }
+      else {
+        window.open("/dashboard", "_self").focus();
+      }
+
+      if (this.state.isError) {
+        this.setState({ countError: this.state.countError + 1 });
+        if (this.state.countError === 2) {
+          this.setState({
+            waitingTime: 30,
+            countError: 0,
+            errorMessage: "Percobaan Login salah 3 kali. Silakan menunggu 30 detik.",
+          });
+          setTimeout(() => {
+            this.setState({ waitingTime: 0, isError: false });
+          }, 30000);
+        }
+      } else {
+      }
+    };
+    return (
+        <div class="limiter">
+          <div class="container-login100">
+            <div class="wrap-login100">
+              <form class="login100-form validate-form" onSubmit={handleSubmit}>
+                <span class="login100-form-title">
+                  Login
+                </span>
+                {this.state.isError ? (
+                    <div class="alert alert-danger" role="alert">
+                      {this.state.errorMessage}
+                    </div>
+                  ) : null}
+                <div class="wrap-input100 validate-input">
+                  <input class="input100" type="text" placeholder="Username" name="inputUsername" required></input>
+                  <span class="focus-input100"></span>
+                  <span class="symbol-input100">
+                    <i class="bi bi-person-circle" aria-hidden="true"></i>
+                  </span>
+                </div>
+
+                <div class="wrap-input100 validate-input">
+                  <input class="input100" type="password" placeholder="Password" id="password-field" name="inputPassword" required></input>
+                  <span class="focus-input100"></span>
+                  <span class="symbol-input100">
+                    <i class="bi bi-file-lock-fill" aria-hidden="true"></i>
+                  </span>
+                </div>
+
+                <div class="container-login100-form-btn">
+                <ReCAPTCHA sitekey="6LdGUhghAAAAADyhHtJB0BXvBqyUBhZmctuZmM2a" onChange={handleClickRecaptcha} ref={(r) => this.captcha = r}/>
+                  <button type="submit" class="login100-form-btn"  disabled={this.state.waitingTime > 0}>
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Login;
